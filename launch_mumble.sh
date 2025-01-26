260c2b5fb28e29a72777bf14d77a4e39add8b09d
rico_jia <ricoruotongjia@gmail.com>
2025-01-26 16:01:01 -0600

    added build on x64 and pulling on rpi

--- launch_mumble.sh
+++ launch_mumble.sh
@@ -4,7 +4,12 @@
 # If we are on rpi, we will launch the arm container. (Real robot)
 # If we are on x86, we will launch the amd64 container. (Simulation)
 
-# TODO: not sure if aarch 64 is what we need
+# Usage:
+# ./launch_mumble.sh [build_rpi]
+#   build_rpi: Optional argument to build the Raspberry Pi container on x86_64 architecture.
+
+
+
 ARCH=$(uname -m)
 CURRENT_DIR=$(dirname $(realpath docker-compose.yml))
 if [ "$ARCH" = "aarch64" ]; then
@@ -13,6 +18,16 @@ if [ "$ARCH" = "aarch64" ]; then
 fi
 
 if [ "$ARCH" = "x86_64" ]; then
+    if [ "$1" = "build_rpi" ]; then
+        docker buildx build \
+        --platform linux/arm64/v8 \
+        --build-arg WORKDIRECTORY=/home/mumble_physical_runtime \
+        -t ricojia/mumble_physical_runtime_rpi:latest \
+        -f ./mumble_physical_runtime/Dockerfile_mumble_physical_runtime \
+        ./mumble_physical_runtime \
+        --push
+    else
+        CURRENT_DIR=$CURRENT_DIR docker compose --profile amd up --build  # TODO: to change to arm
+    fi
     # docker compose --profile amd64 up -d
-    CURRENT_DIR=$CURRENT_DIR docker compose --profile amd up --build  # TODO: to change to arm
 fi
