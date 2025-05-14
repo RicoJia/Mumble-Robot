77ba6325b8d6e57e7fedbc29f32d2669bc66c7ca
rico_jia <ricoruotongjia@gmail.com>
2025-05-14 17:03:57 -0500

    imu round 1

--- mumble_onboard/halo/tests/test_direct_3d_ndt_lo.cpp
+++ mumble_onboard/halo/tests/test_direct_3d_ndt_lo.cpp
@@ -1,4 +1,5 @@
 // ./build/mumble_onboard/halo/test_direct_3d_ndt_lo --stopping_msg_index 1200
+// ./build/mumble_onboard/halo/test_direct_3d_ndt_lo --bag_path src/mumble_onboard/test_bags/rosbag2_2025_05_14-18_48_45
 // To profile: perf record -F 999 --call-graph dwarf -g -- ./build/mumble_onboard/halo/test_direct_3d_ndt_lo  --stopping_msg_index 30
 #include <gtest/gtest.h>
 #include <iostream>
@@ -6,9 +7,11 @@
 #include <halo/common/halo_io.hpp>
 #include <halo/common/sensor_utils.hpp>
 #include <halo/common/debug_utils.hpp>
+#include <halo/common/point_cloud_processing.hpp>
 #include <halo/lo3d/direct_ndt_3d_lo.hpp>
 #include <halo/lo3d/incremental_ndt_3d_lo.hpp>
 #include <gflags/gflags.h>
+#include "sensor_msgs/msg/point_cloud2.hpp"
 
 DEFINE_string(bag_path, "./data/ulhk/test2.txt", "path to rosbag");
 DEFINE_string(dataset_type, "ULHK", "NCLT/ULHK/KITTI/WXB_3D");
@@ -18,44 +21,66 @@ DEFINE_bool(display_map, true, "display map?");
 DEFINE_int64(stopping_msg_index, 0, "0 means no limit, otherwise stop at this message index");
 DEFINE_int64(start_visualize_msg_index, 0, "start visualization from this index");
 
-TEST(DIRECT3DNDTTest, test_direct_3d_ndt) {
-    halo::DirectNDT3DLO::DirectNDT3DLOOptions options;
-    options.display_map = FLAGS_display_map;
-    halo::DirectNDT3DLO direct_ndt_lo(options);
-    TextIO text_io(FLAGS_bag_path, FLAGS_stopping_msg_index);
-    int num_msgs = 0;
-    text_io.register_callback(
-        "ulhk_3d",
-        [&](std::stringstream &ss) {
-            auto scan_msg   = halo::TextIO::convert_2_pointcloud2(ss);
-            auto scan_cloud = halo::convert_2_pclcloud_xyz_i(scan_msg);
-            std::cout << "=================================num_msgs: " << num_msgs << ", ptr size: " << scan_cloud->points.size() << std::endl;
-            std::cout << "FLAGS_start_visualize_msg_index: " << FLAGS_start_visualize_msg_index << std::endl;
-            bool visualize = FLAGS_start_visualize_msg_index < num_msgs;
-            direct_ndt_lo.add_scan(scan_cloud, visualize);
-            num_msgs++;
-        });
-    text_io.spin();
-}
+// TEST(DIRECT3DNDTTest, test_direct_3d_ndt) {
+//     halo::DirectNDT3DLO::DirectNDT3DLOOptions options;
+//     options.display_map = FLAGS_display_map;
+//     halo::DirectNDT3DLO direct_ndt_lo(options);
+//     TextIO text_io(FLAGS_bag_path, FLAGS_stopping_msg_index);
+//     int num_msgs = 0;
+//     text_io.register_callback(
+//         "ulhk_3d",
+//         [&](std::stringstream &ss) {
+//             auto scan_msg   = halo::TextIO::convert_2_pointcloud2(ss);
+//             auto scan_cloud = halo::convert_2_pclcloud_xyz_i(scan_msg);
+//             std::cout << "=================================num_msgs: " << num_msgs << ", ptr size: " << scan_cloud->points.size() << std::endl;
+//             std::cout << "FLAGS_start_visualize_msg_index: " << FLAGS_start_visualize_msg_index << std::endl;
+//             bool visualize = FLAGS_start_visualize_msg_index < num_msgs;
+//             direct_ndt_lo.add_scan(scan_cloud, visualize);
+//             num_msgs++;
+//         });
+//     text_io.spin();
+// }
 
-TEST(DIRECT3DNDTTest, test_incremental_3d_ndt) {
+TEST(DIRECT3DNDTTest, test_incremental_3d_ndt_bag_test) {
     halo::IncrementalNDTLO::Options options;
     options.display_map = FLAGS_display_map;
     halo::IncrementalNDTLO inc_ndt_3d_lo(options);
-    TextIO text_io(FLAGS_bag_path, FLAGS_stopping_msg_index);
+    ROS2BagIo bag_io(FLAGS_bag_path, FLAGS_stopping_msg_index);
     int num_msgs = 0;
-    text_io.register_callback(
-        "ulhk_3d",
-        [&](std::stringstream &ss) {
-            auto scan_msg   = halo::TextIO::convert_2_pointcloud2(ss);
-            auto scan_cloud = halo::convert_2_pclcloud_xyz_i(scan_msg);
+    bag_io.register_callback<sensor_msgs::msg::PointCloud2>(
+        "/tof_sensor/points",
+        [&](std::shared_ptr<sensor_msgs::msg::PointCloud2> scan_msg) {
+            auto scan_cloud = halo::convert_2_pclcloud_xyz_i(*scan_msg);
             std::cout << "=================================num_msgs: " << num_msgs << ", ptr size: " << scan_cloud->points.size() << std::endl;
             bool visualize = FLAGS_start_visualize_msg_index < num_msgs;
             inc_ndt_3d_lo.add_scan(scan_cloud, visualize);
             num_msgs++;
-        });
-    text_io.spin();
+        }
+    );
+    bag_io.spin();
+    if (!FLAGS_display_map) {
+        inc_ndt_3d_lo.save_map("test_incremental_3d_ndt.pcd");
+    }
 }
+
+// TEST(DIRECT3DNDTTest, test_incremental_3d_ndt) {
+//     halo::IncrementalNDTLO::Options options;
+//     options.display_map = FLAGS_display_map;
+//     halo::IncrementalNDTLO inc_ndt_3d_lo(options);
+//     TextIO text_io(FLAGS_bag_path, FLAGS_stopping_msg_index);
+//     int num_msgs = 0;
+//     text_io.register_callback(
+//         "ulhk_3d",
+//         [&](std::stringstream &ss) {
+//             auto scan_msg   = halo::TextIO::convert_2_pointcloud2(ss);
+//             auto scan_cloud = halo::convert_2_pclcloud_xyz_i(scan_msg);
+//             std::cout << "=================================num_msgs: " << num_msgs << ", ptr size: " << scan_cloud->points.size() << std::endl;
+//             bool visualize = FLAGS_start_visualize_msg_index < num_msgs;
+//             inc_ndt_3d_lo.add_scan(scan_cloud, visualize);
+//             num_msgs++;
+//         });
+//     text_io.spin();
+// }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
